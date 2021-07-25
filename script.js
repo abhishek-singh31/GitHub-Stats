fbcc12069043e021b8889a140b40eef1617c4532
Abhishek Pratap Singh <apsingh394@gmail.com>
2021-07-26 01:57:50 +0530

    added reset function to reset everything

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btn=document.getElementById("btn");
 const user=document.getElementById("user");
 const card=document.querySelector(".card");
 const dp=document.getElementById("dp");
+const bio=document.getElementById("bio");
 const username=document.getElementById("username");
 const followers=document.getElementById("followers");
 const following=document.getElementById("following");
@@ -21,9 +22,13 @@ btn.addEventListener("click", (e)=>{
             }
         })
         .then((data) => {
+            reset();
             card.classList.add('show');
             dp.src=data['avatar_url'];
             username.innerHTML=data['name'];
+            if(data['bio']!=null){
+                bio.innerHTML=data['bio'];
+            }
             username.href=data['html_url'];
             followers.innerHTML=data['followers'];
             document.querySelector("#followers-count").innerHTML="Followers";
@@ -33,6 +38,24 @@ btn.addEventListener("click", (e)=>{
             document.querySelector("#repo-count").innerHTML="Repositories";
             dp.hidden=false;
         })
-        .catch(err => alert("Enter correct username"));
+        .catch(err => {
+            reset();
+            alert("Enter correct username");
+        });
     user.value="";
 })
+
+function reset(){
+    card.classList.remove('show');
+    dp.hidden=true;
+    dp.src="";
+    username.innerHTML="";
+    bio.innerHTML="";
+    username.href="";
+    followers.innerHTML="";
+    document.querySelector("#followers-count").innerHTML="";
+    following.innerHTML="";
+    document.querySelector("#following-count").innerHTML="";
+    repo.innerHTML="";
+    document.querySelector("#repo-count").innerHTML="";
+}
